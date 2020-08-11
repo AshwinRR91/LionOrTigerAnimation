5e4dc507b8ee48a12ce14e039f191ba01465bc82
Ashwin <achchu.rai@gmail.com>
2020-08-11 14:03:49 +0530

    Initial commit of Lion or Tiger Animation

---
+++ app/src/main/java/com/mystartup/lionortigeranimation/MainActivity.java
@@ -0,0 +1,91 @@
+package com.mystartup.lionortigeranimation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    String playerOne;
+    String playerTwo;
+    enum Player{
+
+        ONE, TWO
+    }
+    TextView winnerText;
+
+    Player currentPlayer = Player.ONE;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void imageIsTapped(View view){
+
+        ImageView img = (ImageView) view;
+        if(currentPlayer == Player.ONE){
+            img.setImageResource(R.drawable.tiger);
+            img.animate().rotation(360f).setDuration(2000);
+            String tag = img.getTag().toString();
+            checkIfPlayerOneWins(tag);
+            currentPlayer = Player.TWO;
+        }
+        else  if( currentPlayer == Player.TWO){
+
+
+            img.setImageResource(R.drawable.lion);
+            img.animate().rotation(360f).setDuration(2000);
+            String tag = img.getTag().toString();
+            checkIfPlayerTwoWins(tag);
+            currentPlayer = Player.ONE;
+        }
+
+
+    }
+
+    public void checkIfPlayerOneWins(String tag) {
+        playerOne += tag;
+
+        if((playerOne.contains("a")&&playerOne.contains("b")&&playerOne.contains("c"))
+                ||(playerOne.contains("a")&&playerOne.contains("d")&&playerOne.contains("g"))
+        ||(playerOne.contains("b")&&playerOne.contains("e")&&playerOne.contains("h"))
+                ||(playerOne.contains("c")&&playerOne.contains("f")&&playerOne.contains("i"))
+                ||(playerOne.contains("d")&&playerOne.contains("e")&&playerOne.contains("f"))
+                ||(playerOne.contains("g")&&playerOne.contains("h")&&playerOne.contains("i"))
+        )
+
+        {
+            winnerText = findViewById(R.id.winnerText);
+            winnerText.setText("Player One Wins");
+        }
+
+
+    }
+    public void checkIfPlayerTwoWins(String tag){
+        playerTwo += tag;
+
+        if((playerTwo.contains("a")&&playerTwo.contains("b")&&playerTwo.contains("c"))
+                ||(playerTwo.contains("a")&&playerTwo.contains("d")&&playerTwo.contains("g"))
+                ||(playerTwo.contains("b")&&playerTwo.contains("e")&&playerTwo.contains("h"))
+                ||(playerTwo.contains("c")&&playerTwo.contains("f")&&playerTwo.contains("i"))
+                ||(playerTwo.contains("d")&&playerTwo.contains("e")&&playerTwo.contains("f"))
+                ||(playerTwo.contains("g")&&playerTwo.contains("h")&&playerTwo.contains("i"))
+        )
+
+        {
+            winnerText = findViewById(R.id.winnerText);
+            winnerText.setText("Player Two Wins");
+        }
+
+
+    }
+
+}
